cf5449a09461380163372e75831d44e040347bd3
david.rusycki <davidrusyck@gmail.com>
2023-07-19 20:00:32 -0300

    feat: init project

---
+++ src/main/java/com/doceasy/backend/DocEasyBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.doceasy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocEasyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocEasyBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doceasy/backend/DocEasyBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doceasy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocEasyBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
